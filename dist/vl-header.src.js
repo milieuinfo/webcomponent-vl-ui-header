03073f4ee011efb64ed6fec2675f28e1eaa04ce0
coemans <tom.coemans@gmail.com>
2020-09-23 19:41:28 +0200

    bundler support (#34)

--- dist/vl-header.src.js
+++ dist/vl-header.src.js
@@ -1,5 +1,10 @@
-import {vlElement, define} from 'vl-ui-core';
-import 'https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/node_modules/@govflanders/vl-widget-polyfill/dist/index.js';
+import {vlElement, define, awaitScript} from 'vl-ui-core';
+
+awaitScript('vl-header', 'https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/node_modules/@govflanders/vl-widget-polyfill/dist/index.js').then(() => {
+  define('vl-header', VlHeader);
+}).catch(() => {
+  define('vl-header', VlHeader);
+});
 
 /**
  * VlHeader
